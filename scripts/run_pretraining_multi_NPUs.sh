7e23d599a0131f0e2a5c2155a77801c401df35dc
Weidong Du <3170100800@zju.edu.cn>
2023-09-07 11:50:57 +0800

    Update run_pretraining_multi_NPUs.sh

--- scripts/run_pretraining_multi_NPUs.sh
+++ scripts/run_pretraining_multi_NPUs.sh
@@ -5,7 +5,7 @@ path_to_llama_model=/home/username/MODEL/$model_name
 path_to_pt_checkpoint=/home/username/CHECKPOINT/$model_name/pt
 mkdir -p $path_to_pt_checkpoint
 
-INF_NAN_MODE_ENABLE=1 accelerate launch scr/train_bash.py \
+INF_NAN_MODE_ENABLE=1 accelerate launch src/train_bash.py \
     --stage pt \
     --model_name_or_path $path_to_llama_model \
     --do_train \
