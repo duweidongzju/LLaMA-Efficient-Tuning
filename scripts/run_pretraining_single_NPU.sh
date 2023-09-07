00413f9b68609c369400a8d8c0639d25b0d1eeb1
Weidong Du <3170100800@zju.edu.cn>
2023-09-07 11:49:51 +0800

    Update run_pretraining_single_NPU.sh

--- scripts/run_pretraining_single_NPU.sh
+++ scripts/run_pretraining_single_NPU.sh
@@ -3,13 +3,13 @@
 model_name=llama-2-7b-hf
 path_to_llama_model=/home/username/MODEL/$model_name
 path_to_pt_checkpoint=/home/username/CHECKPOINT/$model_name/pt
-mkdir -p path_to_pt_checkpoint
+mkdir -p $path_to_pt_checkpoint
 
-INF_NAN_MODE_ENABLE=1 python scr/train_bash.py
+INF_NAN_MODE_ENABLE=1 python scr/train_bash.py \
     --stage pt \
     --model_name_or_path $path_to_llama_model \
     --do_train \
-    --dataset wiki_domo \
+    --dataset wiki_demo \
     --template default \
     --finetuning_type lora \
     --lora_target q_proj,v_proj \
